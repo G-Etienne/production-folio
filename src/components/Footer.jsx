a28ba9b987c58f39f0248091917cd243df8a6b70
G-etienne <ginetetienne2@gmail.com>
2023-10-12 13:09:15 -0400

    change rules for fixing bugs with header and position screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 //------------------------------------------------------
+import { useLocation } from "react-router-dom";
+//------------------------------------------------------
 // //style
 // import "../styles/layout/_footer.scss";
 // import "../styles/layout/media_queries/_footerMediaQueries.scss";
@@ -8,9 +10,14 @@
 import Desk from "./desk/Desk";
 //------------------------------------------------------
 function Footer() {
+    const location = useLocation();
+    const page = location.pathname.slice(
+        location.pathname.lastIndexOf("/") + 1,
+    );
+
     return (
         <>
-            <footer className="footer">
+            <footer className={`footer ${page}`}>
                 <Desk />
             </footer>
         </>
